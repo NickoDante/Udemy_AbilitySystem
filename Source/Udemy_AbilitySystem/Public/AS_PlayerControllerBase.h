808cf69f7a0b83f125fc5c0194eed81847b17dab
NickoDante <nickodante24@gmail.com>
2020-10-23 11:04:07 -0500

    ** Creating Player Controller, and adding Movement and Jump to the character from there.

---
+++ Source/Udemy_AbilitySystem/Public/AS_PlayerControllerBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "AS_PlayerControllerBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UDEMY_ABILITYSYSTEM_API AAS_PlayerControllerBase : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
